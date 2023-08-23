4cbdcfcd35223f1980e5a8cab5ea5ac2bc507947
saepulmalik <saepulalmalik@gmail.com>
2023-08-23 10:05:43 +0700

    feat : cart count

--- src/components/templates/header/Header.tsx
+++ src/components/templates/header/Header.tsx
@@ -5,7 +5,7 @@ import { HomeIcon } from "@heroicons/react/20/solid";
 import useHeader from "./_hooks/useHeader";
 
 const Header = () => {
-  const { handleCLickBack } = useHeader();
+  const { handleCLickBack, cartCount } = useHeader();
   return (
     <header className='fixed top-0 left-0 p-4 z-40 w-full flex justify-between bg-black bg-opacity-50'>
       <div
@@ -15,7 +15,12 @@ const Header = () => {
         <HomeIcon className=' h-10 w-10' />
         <h1 className='font-bold  text-3xl select-none'>MovieRent</h1>
       </div>
-      <ShoppingCartIcon className='text-white h-10 w-10' />
+      <div className='relative'>
+        <div className='absolute top-0 right-0 w-5 h-5 rounded-full bg-red-500 text-white text-xs flex justify-center items-center'>
+          <span>{cartCount}</span>
+        </div>
+        <ShoppingCartIcon className='text-white h-10 w-10' />
+      </div>
     </header>
   );
 };
